82e9f596f5eea0b6e6ecc8de758bed12b2e99b5c
heydarRasoul <heydar@devpipeline.com>
2025-07-18 16:13:50 -0600

    Update script-3.js

--- node-challenges-combined/script-3.js
+++ node-challenges-combined/script-3.js
@@ -1,8 +1,11 @@
 const colItem = document.querySelector(".col-1");
 colItem.style.backgroundColor = "#ff9c33";
+colItem.style.color = "#5e43e3";
 
 const colItem2 = document.querySelector(".col-2");
 colItem2.style.backgroundColor = "#7df28f";
+colItem2.style.color = "#e34368";
 
 const colItem3 = document.querySelector(".col-3");
 colItem3.style.backgroundColor = "#c85ab2";
+colItem3.style.color = "#25a913";
